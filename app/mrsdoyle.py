23f74645596a9f825b3e0825ca8b28501a17e604
Adam Clements <adam@touchtype-online.com>
2011-09-25 23:58:01 +0100

    Added some variety to the responses Mrs Doyle gives

--- app/mrsdoyle.py
+++ app/mrsdoyle.py
@@ -20,6 +20,27 @@ roster = set([])
 drinkers = set([])
 teacountdown = False
 
+# Trigger words
+TRIGGER_HELLO = r"hi|hello|morning|afternoon|evening"
+TRIGGER_YES = r"yes|yeh|ya|booyah|ok|please|totally|definitely|absolutely|yeah|yup|affirmative|yarr|yah|please|sure"
+TRIGGER_TEA = r"cuppa|tea|brew|the good stuff|"
+
+# Responses
+GREETING = set(["Well hello dear", "Top o' the mornin' to ya", "Hello", "Hi", "Good morning father", "Beautiful day outside isn't it?", "I'm feeling fat, and sassy"])
+WANT_TEA = set (["Will you have a cup of tea?", "Will you have a cup of tea father?", "We were just about to have a cup of tea, will you join us?", "Join us in a cup of tea?", "Tea perchance?", "Could I interest you in a brew?", "Hot beverage?"])
+NOBACKOUT = set(["Your fate is secured, back out now and the whole system crumbles. You *will* have tea.", "You are already in the list of tea drinkers. There's no getting out of it now."])
+AHGRAND = set(["Ah, grand! I'll wait a couple of minutes and see if anyone else wants one", "Champion.", "You won't regret it!", "Wonderful!", "I'm so glad!"])
+AH_GO_ON = set(["Ah, go on! Won't you just have a cup", "There's childers in Africa who can't even have tea. Won't you just have a cup", "Ah go on go on go on", "Go on, go on, go on", "It's no bother, really", "It would make me so happy if you'd just have a cup"])
+GOOD_IDEA = set(["Fantastic idea, I'll see who else wants one and get back to you in a couple of minutes", "I was just about to suggest the same thing. I'll see who else wants one", "Coming right up... in a couple of minutes"])
+ON_YOUR_OWN = set(["You're on your own I'm afraid, nobody else wants one!", "What? Well, this is embarrassing... nobody else seems to want tea :(", "Well, this is practically unheard of, I could convince *nobody* to have a cup", "Sad times indeed, tea for one today"])
+WELL_VOLUNTEERED = set(["Well volunteered! The following other people want tea!", "Be a love and put the kettle on would you?", "You know what, I think it's your turn to make the tea now I think about it.", "Polly put the kettle on, kettle on, kettle on. You are Polly in this game.", "The tea was a lie, you'll have to make it for yourself", "Why not stretch those weary legs and have a wander over to the kitchen. Say, while you're there...."])
+OTHEROFFERED = set([" has been kind enough to make the tea, I'd do it myself only I don't have arms", " has been kind enough to make the tea", " kindly offered to make the tea", " is about to selflessly put the kettle on", " is today's lucky tea lady", " will soon bring you a warm fuzzy feeling in a cup"])
+HUH = set(["I don't understand what you're saying...", "If it's not about tea, I'm afraid I'm not really interested...", "Pardon?", "Beg pardon?", "Hm?", "Umm.....", "Pancakes.", "I fail to see the relevance...", "Is there something I can do for you?"])
+
+
+def send_random(recipient, choices, prefix=""):
+  xmpp.send_message(recipient, prefix + random.sample(choices, 1)[0])
+
 class XmppHandler(xmpp_handlers.CommandHandler):
   """Handler class for all XMPP activity."""
 
@@ -36,7 +57,16 @@ class XmppHandler(xmpp_handlers.CommandHandler):
     message.reply("CountingDown " + str(teacountdown))
 
   def text_message(self, message=None):
-    global someone
+    global NOBACKOUT
+    global GREETING
+    global AHGRAND
+		global GOOD_IDEA
+		global HUH
+		
+		global TRIGGER_HELLO
+		global TRIGGER_YES
+		global TRIGGER_TEA
+    
     global roster
     global teacountdown
     global drinkers
@@ -46,16 +76,16 @@ class XmppHandler(xmpp_handlers.CommandHandler):
     
     if teacountdown:
       if fromaddr in drinkers:
-        message.reply("You are already in the list of tea drinkers. there's no getting out of it now.")
+        send_random(fromaddr, NOBACKOUT)
         return
     
-      if re.search(r"yes|yeh|ya|booyah|ok|please|totally|definitely|absolutely|yeah|yup|affirmative|yarr|yah|please|sure", message.body, re.IGNORECASE):
+      if re.search(TRIGGER_YES, message.body, re.IGNORECASE):
         drinkers.add(fromaddr)
-        message.reply("Ah, grand! I'll wait a couple of minutes and see if anyone else wants one")
+        send_random(fromaddr, AH_GRAND)
       else:
-        message.reply("Ah, go on! Won't you just have a cup")
-    elif re.search(r"cuppa|tea|brew", message.body, re.IGNORECASE):
-      message.reply("Fantastic idea, I'll see who else wants one and get back to you in a couple of minutes")
+        send_random(fromaddr, AH_GO_ON)
+    elif re.search(TRIGGER_TEA, message.body, re.IGNORECASE):
+      send_random(fromaddr, GOOD_IDEA)
       drinkers.add(fromaddr)
       
       for person in roster:
@@ -66,14 +96,14 @@ class XmppHandler(xmpp_handlers.CommandHandler):
       doittask.add()
       teacountdown = True
       
-    elif re.search(r"hi|hello|morning|afternoon|evening", message.body, re.IGNORECASE):
-      message.reply("Top o' the morning to ya")
+    elif re.search(TRIGGER_HELLO, message.body, re.IGNORECASE):
+      send_random(fromaddr, GREETING)
     else:
-      message.reply("I don't understand what you're saying.... ")
+      send_random(fromaddr, HUH)
     
   def attemptsend_command(self, message=None):    
-		global teacountdown
-		global roster
+    global teacountdown
+    global roster
     for person in roster:
       xmpp.send_presence(jid=person, presence_type = xmpp.PRESENCE_TYPE_PROBE)
     
@@ -83,34 +113,40 @@ class XmppHandler(xmpp_handlers.CommandHandler):
     
 class DoThis(webapp.RequestHandler):
     def post(self):
+		  global ON_YOUR_OWN
+			global WELL_VOLUNTEERED
+			global OTHEROFFERED
+			
       global drinkers
       global teacountdown
       teamaker = random.sample(drinkers, 1)[0]
       
       if drinkers == set([teamaker]):
-        xmpp.send_message(teamaker, "Nobody else wants one, you're on your own I'm afraid!")
+			  send_random(teamaker, ON_YOUR_OWN)
       else:      
         for person in drinkers:
           if person == teamaker:
-            xmpp.send_message(person, "Well volunteered! The following other people want tea:")
+            send_random(person, WELL_VOLUNTEERED)
             for drinker in drinkers:
               if drinker != person:
                 xmpp.send_message(person, drinker.split("@")[0].title())
           else:
-            xmpp.send_message(person, teamaker.split("@")[0].title() + " has been kind enough to offer to make the tea, I'd do it myself only I don't have arms.")
+            send_random(person, OTHEROFFERED, teamaker.split("@")[0].title())
         
       teacountdown = False     
       drinkers = set([])
 
 class Register(webapp.RequestHandler):
     def post(self):
-      global roster
-			global teacountdown
+      global WANT_TEA
+			global roster
+      global teacountdown
+			
       person = self.request.get('from').split("/")[0]
       roster.add(person)
-			
-			if(teacountdown):
-				xmpp.send_message(person, "Will you have a cup of tea??")  
+      
+      if(teacountdown):
+			  send_random(person, WANT_TEA)
       
 def main():
   app = webapp.WSGIApplication([
