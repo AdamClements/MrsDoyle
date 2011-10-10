4ed867a1d4d2b8332434d7e86c674968ec9ce54f
Adam Clements <adam@touchtype-online.com>
2011-10-11 00:12:34 +0100

    Rejigged stats so we keep totals but also individuals' drinking times along with the round sizes and times. Again this is for ease of exporting/querying the data

--- app/stats.py
+++ app/stats.py
@@ -1,25 +1,31 @@
 from google.appengine.ext import db
 
-class PerUserStats(db.Model):
-  date = db.DateTimeProperty(auto_now_add=True)
+class PerUserTotals(db.Model):
   cupsMade = db.IntegerProperty(default=0)
   cupsDrunk = db.IntegerProperty(default=0)
+  
+class PerUserStats(db.Model):
+  date = db.DateTimeProperty(auto_now_add=True)
+  drinker = db.StringProperty(required=True)  
   
 class PerRoundStats(db.Model):
   date = db.DateTimeProperty(auto_now_add=True)
   maker = db.StringProperty(required=True)
   cups = db.IntegerProperty(required=True)
   
-def statDrinker(name):
-  drinkerStats = PerUserStats.get_or_insert(key_name=name)
-  drinkerStats.cupsDrunk = drinkerStats.cupsDrunk + 1
+def statDrinker(name, maker=False):
+  totals = PerUserTotals.get_or_insert(key_name=name)
+  totals.cupsDrunk = totals.cupsDrunk + 1
+  if maker:
+    totals.cupsMade = totals.cupsMade + 1  
+  totals.put()
+  
+  drinkerStats = PerUserStats(drinker=name)
   drinkerStats.put()
   
+  
 def statMaker(name):
-  drinkerStats = PerUserStats.get_or_insert(key_name=name)
-  drinkerStats.cupsDrunk = drinkerStats.cupsDrunk + 1
-  drinkerStats.cupsMade = drinkerStats.cupsMade + 1
-  drinkerStats.put()
+  statDrinker(name, True)  
   
 def statRound(pName, pCups):
   newRound = PerRoundStats(maker=pName, cups=pCups)
